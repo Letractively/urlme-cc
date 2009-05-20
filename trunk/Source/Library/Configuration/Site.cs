73f9e04249652133317466a0d71c2a47b9e9c758
ihdavis@gmail.com <ihdavis@gmail.com@c0190cfc-3530-11de-9f24-65e390db57b8>
2009-05-20 04:22:24 +0000

    Restructuring to Assemblies, Source, and Tools folder hierarchy per http://wiki.salemweb.net/standards;site-folder-and-file-architecture?s[]=assemblies; initial commit to SubVersion.

---
+++ trunk/Source/Library/Configuration/Site.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace Library.Configuration
+{
+    public class Site
+    {
+        public static string Logo
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["SiteLogo"];
+            }
+        }
+        public static string ApplicationUrlRoot
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["ApplicationUrlRoot"];
+            }
+        }
+        public static int RedirectDelaySeconds
+        {
+            get
+            {
+                return int.Parse(ConfigurationManager.AppSettings["RedirectDelaySeconds"]);
+            }
+        }
+        public static int SiteUpdateNewNoticeDaySpan
+        {
+            get
+            {
+                return int.Parse(ConfigurationManager.AppSettings["SiteUpdateNewNoticeDaySpan"]);
+            }
+        }
+    }
+}
---
+++ trunk/Source/Site/Login.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+
+namespace UrlMe.cc
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                // load site updates
+                gvSiteUpdates.DataSource = Library.Data.SiteData.GetSiteUpdatesSiteCD("URLME");
+                gvSiteUpdates.DataBind();
+            }
+        }
+
+        protected void btnSignIn_Click(object sender, EventArgs e)
+        {
+            int userID = Library.Data.UserData.AuthenticateUser(txtEmail.Text,txtPassword.Text);
+            if (userID != -1)
+                FormsAuthentication.RedirectFromLoginPage(userID.ToString() + "|" + txtEmail.Text, chkRememberMe.Checked);
+            else
+                Response.Write("Wrong email and/or password. Please try again.");
+        }
+        protected void btnCreateAccount_Click(object sender, EventArgs e)
+        {
+            int userID = Library.Data.UserData.NewUser(txtEmail.Text, txtPassword.Text,txtPasswordHint.Text);
+            if (userID != -1)
+                FormsAuthentication.RedirectFromLoginPage(userID.ToString() + "|" + txtEmail.Text, chkRememberMe.Checked);
+            else
+                Response.Write("Something broke on account creation. Undo.");
+        }
+        #region GetNewNotice
+        // put this in Utils
+        public string GetNewNotice(DateTime dateUpdated)
+        {
+            TimeSpan ts = dateUpdated - DateTime.Now;
+            if (Math.Abs(ts.Days) < Library.Configuration.Site.SiteUpdateNewNoticeDaySpan)
+                return "<b><font color=\"green\">New!</font></b>&nbsp;";
+            else
+                return "";
+        }
+        #endregion
+    }
+}

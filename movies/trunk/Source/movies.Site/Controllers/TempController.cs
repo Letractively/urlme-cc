8508eaad38c9d2aad2232f9cb7788c20211bc052
ihdavis@gmail.com <ihdavis@gmail.com@c0190cfc-3530-11de-9f24-65e390db57b8>
2011-10-18 01:20:26 +0000

    initial commit to svn

---
+++ movies/trunk/Source/movies.Site/Controllers/TempController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace movies.Site.Controllers
+{
+    public class TempController : Controller
+    {
+        //
+        // GET: /Temp/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}

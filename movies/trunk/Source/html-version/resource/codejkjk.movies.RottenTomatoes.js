5519a2d8480e2071a1a5e54758eed259c6f07a52
ihdavis@gmail.com <ihdavis@gmail.com@c0190cfc-3530-11de-9f24-65e390db57b8>
2011-10-26 20:51:39 +0000

    lot of work with getting theater view in place

--- movies/trunk/Source/html-version/resource/codejkjk.movies.RottenTomatoes.js
+++ movies/trunk/Source/html-version/resource/codejkjk.movies.RottenTomatoes.js
@@ -5,21 +5,34 @@ codejkjk.movies.RottenTomatoes = {
     BaseUrl: "http://api.rottentomatoes.com/api/public/v1.0/",
     GetInTheatersMovies: function (callback) {
         var url = String.format("{0}lists/movies/in_theaters.json?page_limit=16&page=1&country=us&apikey={1}", codejkjk.movies.RottenTomatoes.BaseUrl, codejkjk.movies.RottenTomatoes.ApiKey);
-        codejkjk.movies.RottenTomatoes.AjaxGet(url, callback);
+        codejkjk.movies.RottenTomatoes.AjaxGetMovies(url, callback);
     },
     GetBoxOfficeMovies: function (callback) {
         var url = String.format("{0}lists/movies/box_office.json?page_limit=16&page=1&country=us&apikey={1}", codejkjk.movies.RottenTomatoes.BaseUrl, codejkjk.movies.RottenTomatoes.ApiKey);
-        codejkjk.movies.RottenTomatoes.AjaxGet(url, callback);
+        codejkjk.movies.RottenTomatoes.AjaxGetMovies(url, callback);
     },
     SearchMovies: function (q, callback) {
         var url = String.format("{0}movies.json?page_limit=16&page=1&country=us&apikey={1}&q={2}", codejkjk.movies.RottenTomatoes.BaseUrl, codejkjk.movies.RottenTomatoes.ApiKey, encodeURI(q));
-        codejkjk.movies.RottenTomatoes.AjaxGet(url, callback);
+        codejkjk.movies.RottenTomatoes.AjaxGetMovies(url, callback);
     },
-    AjaxGet: function (url, callback) {
+    GetMovie: function (movieId, callback) {
+        // if (movieId == 771092231) { alert('about to ajax get'); };
+        var url = String.format("{0}movies/{1}.json?apikey={2}", codejkjk.movies.RottenTomatoes.BaseUrl, movieId, codejkjk.movies.RottenTomatoes.ApiKey);
+        codejkjk.movies.RottenTomatoes.AjaxGetMovie(url, callback);
+    },
+    AjaxGetMovies: function (url, callback) {
         $.ajax({
             url: url,
             dataType: "jsonp",
             success: function (response) { return callback(response.movies); },
+            error: function () { return null; }
+        });
+    },
+    AjaxGetMovie: function (url, callback) {
+        $.ajax({
+            url: url,
+            dataType: "jsonp",
+            success: function (response) { return callback(response); },
             error: function () { return null; }
         });
     }

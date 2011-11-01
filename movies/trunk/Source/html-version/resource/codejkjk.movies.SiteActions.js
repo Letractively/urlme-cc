c4f534b48b8d4f38e90fe0058c1d6611d61e1283
ihdavis <ihdavis@gmail.com>
2011-11-01 20:00:00 +0000

    collapse/expand state on theaters is stored in localStorage awesomeness

--- movies/trunk/Source/html-version/resource/codejkjk.movies.SiteActions.js
+++ movies/trunk/Source/html-version/resource/codejkjk.movies.SiteActions.js
@@ -4,16 +4,27 @@ codejkjk.movies.SiteActions = {
     WireCollapsers: function () {
         $(".collapser").unbind().click(function (e) {
             e.preventDefault();
+
             var collapser = $(this);
             var collapsee = $($(collapser.attr("collapsee")));
-//            if (collapsee.hasClass("collapsed")) {
-//                collapsee.fadeIn();
-//            } else {
-//                collapsee.fadeOut();
-//            }
-            collapsee.toggle()
+            var collapsedTheaterIds = localStorage.getItem("CollapsedTheaterIds") != null ? localStorage.getItem("CollapsedTheaterIds").split(',') : [];
+            var theaterId = collapser.closest(".theater").attr("id");
+
+            if (collapser.hasClass("collapsed")) {
+                collapsedTheaterIds.removeByElement(theaterId);
+            } else {
+                collapsedTheaterIds.pushIfDoesNotExist(theaterId);
+            }
+
+            collapsee.slideToggle('fast');
             collapser.toggleClass("collapsed").toggleClass("expanded");
             collapser.blur();
+
+            if (collapsedTheaterIds.length > 0) {
+                localStorage.setItem("CollapsedTheaterIds", collapsedTheaterIds.join(','));
+            } else {
+                localStorage.removeItem("CollapsedTheaterIds");
+            }
         });
     }
 };

cc63f4670e17c30615f534405993433b60b281fc
ihdavis <ihdavis@gmail.com>
2011-12-21 18:11:33 +0000

    movies3

--- movies/trunk/Source/html-version/resource/codejkjk.movies.IMDB.js
+++ movies/trunk/Source/html-version/resource/codejkjk.movies.IMDB.js
@@ -3,7 +3,7 @@ registerNS("codejkjk.movies.IMDB");
 codejkjk.movies.IMDB = {
     BaseUrl: "http://www.imdbapi.com/?i=tt",
     GetMovie: function (movieId, callback) {
-        var url = String.format("{0}{1}", codejkjk.movies.IMDB.BaseUrl, movieId);
+        var url = "{0}{1}".format(codejkjk.movies.IMDB.BaseUrl, movieId);
         $.ajax({
             url: url,
             dataType: "jsonp",
@@ -12,11 +12,11 @@ codejkjk.movies.IMDB = {
         });
     },
     GetMovieUrl: function (movieId) {
-        return String.format("http://www.imdb.com/title/tt{0}/", movieId);
+        return "http://www.imdb.com/title/tt{0}/".format(movieId);
     },
     GetParentalGuideUrl: function (movieId) {
         if (movieId.indexOf("tt") != 0)
             movieId = "tt" + movieId;
-        return String.format("http://www.imdb.com/title/{0}/parentalguide", movieId);
+        return "http://www.imdb.com/title/{0}/parentalguide".format(movieId);
     }
 };
--- movies/trunk/Source/html-version/resource/codejkjk.movies.RottenTomatoes.js
+++ movies/trunk/Source/html-version/resource/codejkjk.movies.RottenTomatoes.js
@@ -4,20 +4,19 @@ codejkjk.movies.RottenTomatoes = {
     ApiKey: "pfrwfgnr53tpaydw8pnhrymy",
     BaseUrl: "http://api.rottentomatoes.com/api/public/v1.0/",
     GetInTheatersMovies: function (callback) {
-        var url = String.format("{0}lists/movies/in_theaters.json?page_limit=16&page=1&country=us&apikey={1}", codejkjk.movies.RottenTomatoes.BaseUrl, codejkjk.movies.RottenTomatoes.ApiKey);
+        var url = "{0}lists/movies/in_theaters.json?page_limit=16&page=1&country=us&apikey={1}".format(codejkjk.movies.RottenTomatoes.BaseUrl, codejkjk.movies.RottenTomatoes.ApiKey);
         codejkjk.movies.RottenTomatoes.AjaxGetMovies(url, callback);
     },
     GetBoxOfficeMovies: function (callback) {
-        var url = String.format("{0}lists/movies/box_office.json?page_limit=16&page=1&country=us&apikey={1}", codejkjk.movies.RottenTomatoes.BaseUrl, codejkjk.movies.RottenTomatoes.ApiKey);
+        var url = "{0}lists/movies/box_office.json?page_limit=16&page=1&country=us&apikey={1}".format(codejkjk.movies.RottenTomatoes.BaseUrl, codejkjk.movies.RottenTomatoes.ApiKey);
         codejkjk.movies.RottenTomatoes.AjaxGetMovies(url, callback);
     },
     SearchMovies: function (q, callback) {
-        var url = String.format("{0}movies.json?page_limit=16&page=1&country=us&apikey={1}&q={2}", codejkjk.movies.RottenTomatoes.BaseUrl, codejkjk.movies.RottenTomatoes.ApiKey, encodeURI(q));
+        var url = "{0}movies.json?page_limit=16&page=1&country=us&apikey={1}&q={2}".format(codejkjk.movies.RottenTomatoes.BaseUrl, codejkjk.movies.RottenTomatoes.ApiKey, encodeURI(q));
         codejkjk.movies.RottenTomatoes.AjaxGetMovies(url, callback);
     },
     GetMovie: function (movieId, callback) {
-        // if (movieId == 771092231) { alert('about to ajax get'); };
-        var url = String.format("{0}movies/{1}.json?apikey={2}", codejkjk.movies.RottenTomatoes.BaseUrl, movieId, codejkjk.movies.RottenTomatoes.ApiKey);
+        var url = "{0}movies/{1}.json?apikey={2}".format(codejkjk.movies.RottenTomatoes.BaseUrl, movieId, codejkjk.movies.RottenTomatoes.ApiKey);
         codejkjk.movies.RottenTomatoes.AjaxGetMovie(url, callback);
     },
     AjaxGetMovies: function (url, callback) {

f8104d7c9e97c909d8b4ffbd7f2a2351508a8830
ihdavis <ihdavis@gmail.com>
2012-10-22 14:46:07 +0000

    progress on trailer popup

--- movies/trunk/Source/movies.Site/Scripts/codejkjk/codejkjk.movies.Api.js
+++ movies/trunk/Source/movies.Site/Scripts/codejkjk/codejkjk.movies.Api.js
@@ -79,6 +79,17 @@ codejkjk.movies.Api = {
         var url = "{0}get_rt_movie.html/{1}".format(codejkjk.movies.Api.BaseUrl, rtMovieId);
         codejkjk.movies.Api.AjaxGet(url, callback, 'html', cacheKey);
     },
+    GetTrailerHtml: function (rtMovieId, callback) {
+        // first, check cache
+        var cacheKey = "trailer-html-{0}".format(rtMovieId);
+        var cached = $.cacheItem(cacheKey);
+        if (cached && codejkjk.movies.Defaults.AllowCache) {
+            return callback(cached);
+        }
+
+        var url = "{0}get_trailer.html/{1}".format(codejkjk.movies.Api.BaseUrl, rtMovieId);
+        codejkjk.movies.Api.AjaxGet(url, callback, 'html', cacheKey);
+    },
     GetMovieSimpleHtml: function (rtMovieId, callback) {
         // first, check cache
         var cacheKey = "rt-html-simple-{0}".format(rtMovieId);

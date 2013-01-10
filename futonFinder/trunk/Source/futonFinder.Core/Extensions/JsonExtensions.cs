2d7ab9e58c1779b4deb06ab7a3910ac51ac32d47
ihdavis@gmail.com <ihdavis@gmail.com@c0190cfc-3530-11de-9f24-65e390db57b8>
2013-01-10 02:47:45 +0000

    progress on facebook and google.

---
+++ futonFinder/trunk/Source/futonFinder.Core/Extensions/JsonExtensions.cs
@@ -0,0 +1,85 @@
+﻿using System.Web.Script.Serialization;
+using System.Text;
+using System.IO;
+using System.Xml.Serialization;
+using System;
+
+namespace futonFinder.Core.Extensions
+{
+    public static class JsonExtensions
+    {
+        public static T FromJson<T>(this string json)
+        {
+            return new JavaScriptSerializer().Deserialize<T>(json);
+        }
+
+        public static T FromXml<T>(this string xml)
+        {
+            return (T)FromXml(xml, typeof(T));
+        }
+
+        private static object FromXml(string objectData, Type type)
+        {
+            var serializer = new XmlSerializer(type);
+            object result;
+
+            using (TextReader reader = new StringReader(objectData))
+            {
+                result = serializer.Deserialize(reader);
+            }
+
+            return result;
+        }
+
+        //public static string ToJson(this object obj)
+        //{
+        //    return new JavaScriptSerializer().Serialize(obj);
+        //}
+
+        //public static T FromXml<T>(this string xml)
+        //{
+        //    byte[] bytes = Encoding.UTF8.GetBytes(xml);
+        //    MemoryStream mem = new MemoryStream(bytes);
+        //    return new XmlSerializer(typeof(T)).Deserialize<T>(mem);
+        //}
+
+        //public static string Serialize(this object obj)
+        //{
+        //    MemoryStream mem = new MemoryStream();
+        //    XmlSerializer ser = new XmlSerializer(obj.GetType());
+        //    ser.Serialize(mem, obj);
+        //    ASCIIEncoding ascii = new ASCIIEncoding();
+        //    return ascii.GetString(mem.ToArray());
+        //}
+
+        //public static object Deserialize(Type typeToDeserialize, string xmlString)
+        //{
+        //    byte[] bytes = Encoding.UTF8.GetBytes(xmlString);
+        //    MemoryStream mem = new MemoryStream(bytes);
+        //    XmlSerializer ser = new XmlSerializer(typeToDeserialize);
+        //    return ser.Deserialize(mem);
+        //}
+
+        //public static string JsonSerializeToString(this object obj)
+        //{
+        //    string json = string.Empty;
+        //    DataContractJsonSerializer ser = new DataContractJsonSerializer(obj.GetType());
+        //    using (MemoryStream ms = new MemoryStream())
+        //    {
+        //        ser.WriteObject(ms, obj);
+        //        json = Encoding.UTF8.GetString(ms.ToArray());
+        //    }
+
+        //    return json;
+        //}
+
+        //public static T JsonDeserializeFromString<T>(this string json)
+        //{
+        //    using (MemoryStream ms = new MemoryStream(ASCIIEncoding.UTF8.GetBytes(json)))
+        //    {
+        //        DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(T));
+        //        return (T)ser.ReadObject(ms);
+        //    }
+        //}
+    }
+}
---
+++ futonFinder/trunk/Source/futonFinder.Site/Models/Facebook.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using futonFinder.Core.Extensions;
+using System.Web.Mvc;
+
+namespace futonFinder.Site.Models
+{
+    public class Facebook
+    {
+        public string Facebook_GraphAPI_Token = "https://graph.facebook.com/oauth/access_token?";
+        public string Facebook_GraphAPI_Me = "https://graph.facebook.com/me?";
+        public string AppID = "314488141986761";
+        public string AppSecret = "998f3cd53ee4af3420734d8ee386ce3c";
+        
+        public IDictionary<string, string> GetUserData(string accessCode, string redirectURI)
+        {
+            string token = futonFinder.Core.Net.HttpWebRequest.GetResponse(Facebook_GraphAPI_Token + "client_id=" + AppID + "&redirect_uri=" + HttpUtility.HtmlEncode(redirectURI) + "%3F__provider__%3Dfacebook" + "&client_secret=" + AppSecret + "&code=" + accessCode);
+            if (token == null || token == "")
+            {
+                return null;
+            }
+            string data = futonFinder.Core.Net.HttpWebRequest.GetResponse(Facebook_GraphAPI_Me + "fields=id,name,email,username,gender,link&access_token=" + token.Substring("access_token=", "&"));
+
+            // this dictionary must contains
+            var userData = new Dictionary<string, string>();
+            userData.Add("id", data.Substring("\"id\":\"", "\""));
+            userData.Add("username", data.Substring("username\":\"", "\""));
+            userData.Add("name", data.Substring("name\":\"", "\""));
+            userData.Add("link", data.Substring("link\":\"", "\"").Replace("\\/","/"));
+            userData.Add("gender", data.Substring("gender\":\"", "\""));
+            userData.Add("email", data.Substring("email\":\"", "\"").Replace("\\u0040", "@"));
+            userData.Add("accesstoken", token.Substring("access_token=", "&"));
+            return userData;
+        }
+    }
+}

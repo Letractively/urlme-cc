7ee65ef6890ff1ca20f79d413a14e8e981274548
ihdavis <ihdavis@gmail.com>
2014-06-16 14:38:23 +0000

    koala goodness.

--- urlme/3.0/trunk/Source/urlme.site/App_Start/BundleConfig.cs
+++ urlme/3.0/trunk/Source/urlme.site/App_Start/BundleConfig.cs
@@ -24,15 +24,6 @@ namespace urlme.site
                       "~/Scripts/bootstrap.min.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.min.css",
-                      "~/Content/bootstrap-social.css",
-                      //"~/Content/bootstrap-theme.min.css",
-                      //"~/content/todc-bootstrap.min.css",
-                      "~/scripts/plugins/bootstrap-dialog/bootstrap-dialog.min.css",
-                      "~/scripts/plugins/datatables/datatables.bootstrap.css",
-                      "~/Content/site.css"));
-
             BundleTable.EnableOptimizations = false;
         }
     }
